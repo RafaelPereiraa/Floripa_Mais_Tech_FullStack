86ecd6b66f30ff25fae0a4aa61c7c4f10f087223
unknown <rafaelsilvapereiraa@gmail.com>
2023-02-28 20:22:19 -0300

    Exercício 02

---
+++ Modulo_01/Semana_05/Exercicio_02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FileInfo arquivo = null;
+        string nomeArquivo;
+        try
+        {
+            try
+            {
+                Console.WriteLine("Digite o nome do arquivo: ");
+                nomeArquivo = Console.ReadLine();
+                arquivo = new FileInfo(nomeArquivo);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ocorreu um erro - " + ex.Message);
+                Console.WriteLine("Criando Arquivo Padrão");
+                nomeArquivo = "Default.txt";
+            }            
+            using (StreamWriter stream = File.AppendText(nomeArquivo))
+            {
+                Console.WriteLine("O que deseja escrever no arquivo?");
+                stream.WriteLine(Console.ReadLine());
+            }
+        } catch(Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            Console.Clear();
+            Console.WriteLine("--- Encerrando ---");
+            arquivo = null;
+        }
+
+    }
+}
