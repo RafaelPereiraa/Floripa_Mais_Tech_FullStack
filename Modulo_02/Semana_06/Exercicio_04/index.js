552b7b90c528862ea7846de38bacde3536ad54c1
Rafael <rafaelsilvapereriaa@gmail.com>
2023-06-07 19:19:25 -0300

    Módulo 02 - Semana 6 - Exercício 04

---
+++ Modulo_02/Semana_06/Exercicio_04/index.js
@@ -0,0 +1,21 @@
+import pegarNomesIbge from './ibge.js';
+let lista = document.querySelector('#lista-nomes');
+let listaNomesInvertidos = document.querySelector('#lista-nomes-invertidos');
+
+console.clear();
+
+pegarNomesIbge().then((data) => {
+
+    let filteredData = data.filter((pessoa) => pessoa.nome.toLowerCase().includes("a"))
+
+    for (let pessoa in filteredData) {
+        lista.innerHTML += `<li>${filteredData[pessoa].nome}</li>`;
+    }
+    
+    for (let pessoa in filteredData.reverse()) {
+        listaNomesInvertidos.innerHTML += `<li>${filteredData[pessoa].nome}</li>`;
+    }
+    })
+    .catch((error) => {
+        console.log("Ocorreu um erro:", error);
+    });
