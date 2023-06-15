e6484c43eefe540ea4b5d5ccd9a630572fc1d1b6
Rafael <rafaelsilvapereriaa@gmail.com>
2023-06-15 20:03:57 -0300

    Módulo 02 - Semana 7 - Exercício 03

--- Modulo_02/Semana_07/Exercicios/src/app/home/home.component.ts
+++ Modulo_02/Semana_07/Exercicios/src/app/home/home.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   texto: string = 'teste';
+  numero01: number = 0;
+  numero02: number = 0;
+  resultado: number | undefined;
+
+  somar(){
+    this.resultado = this.numero01 + this.numero02;
+  }
+
+  subtrair(){
+    this.resultado = this.numero01 - this.numero02;
+  }
+
+  multiplicar(){
+    this.resultado = this.numero01 * this.numero02;
+  }
+
+  dividir(){
+    this.resultado = this.numero01 / this.numero02;
+  }
+
+  limpar(){
+    this.numero01 = 0;
+    this.numero02 = 0;
+    this.resultado = undefined;
+  }
+  
 }
