f70d0b8910dea485d0e56456729ab063e468bc5d
Rafael <rafaelsilvapereriaa@gmail.com>
2023-06-16 00:57:05 -0300

    Módulo 02 - Semana 7 - Exercício 04 - Funcionando sem form

--- Modulo_02/Semana_07/Exercicios/src/app/home/home.component.ts
+++ Modulo_02/Semana_07/Exercicios/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,25 @@ export class HomeComponent {
   numero01: number = 0;
   numero02: number = 0;
   resultado: number | undefined;
+  nome: string = '';
+  email: string = '';
+  nomeFilme: string = '';
+  nota: number = 0;
+  formFilme: FormGroup;
+
+  constructor() {
+    this.formFilme = new FormGroup({
+      nome: new FormControl(''),
+      email: new FormControl(''),
+      nomeFilme: new FormControl(''),
+      nota: new FormControl('')
+    });
+  }
+
+  onSubmit(){
+    alert(`Voce avaliou o filme ${this.nomeFilme}!`);
+  }
+
 
   somar(){
     this.resultado = this.numero01 + this.numero02;
@@ -32,5 +52,5 @@ export class HomeComponent {
     this.numero02 = 0;
     this.resultado = undefined;
   }
-  
+
 }
