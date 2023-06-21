743a25012a34e8a7ecb9256e1e535308aeb4f1bb
Rafael <rafaelsilvapereriaa@gmail.com>
2023-06-21 00:04:57 -0300

    Módulo 02 - Semana 8 - Exercício 03 a 10 - Estilização Básica

--- Modulo_02/Semana_08/MiniProjeto/src/assets/static_mock.ts
+++ Modulo_02/Semana_08/MiniProjeto/src/assets/static_mock.ts
@@ -1,12 +1,44 @@
-export const staticMock = [{ 
-    thumb: 'x',
+export const staticMock = [
+  {
+    thumb: '../assets/logo.svg',
     title: 'Notificação 1',
     subtitle: 'Mensagem da notificação 1',
-    data: '01/01/2021'
+    data: '01/01/2021',
   },
-  { 
+  {
     thumb: 'x',
     title: 'Notificação 1',
     subtitle: 'Mensagem da notificação 1',
-    data: '01/01/2021'
-  }];  
+    data: '01/01/2021',
+  },
+  {
+    thumb: 'x',
+    title: 'Notificação 1',
+    subtitle: 'Mensagem da notificação 1',
+    data: '01/01/2021',
+  },
+  {
+    thumb: 'x',
+    title: 'Notificação 1',
+    subtitle: 'Mensagem da notificação 1',
+    data: '01/01/2021',
+  },
+  {
+    thumb: 'x',
+    title: 'Notificação 1',
+    subtitle: 'Mensagem da notificação 1',
+    data: '01/01/2021',
+  },
+  {
+    thumb: 'x',
+    title: 'Notificação 1',
+    subtitle: 'Mensagem da notificação 1',
+    data: '01/01/2021',
+  },
+  {
+    thumb: 'x',
+    title: 'Notificação 1',
+    subtitle: 'Mensagem da notificação 1',
+    data: '01/01/2021',
+  },
+];
